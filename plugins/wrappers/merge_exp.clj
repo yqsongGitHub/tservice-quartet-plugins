b72a102c5e17555ca4cbf5830db5f9a7df7c2ec4
songyueqiang <songyq_hello@163.com>
2021-04-20 10:15:58 +0800

    change gene_id to GENE_ID

--- plugins/wrappers/merge_exp.clj
+++ plugins/wrappers/merge_exp.clj
@@ -6,7 +6,7 @@
 
 (defn sort-exp-data
   [coll]
-  (sort-by :gene_id coll))
+  (sort-by :GENE_ID coll))
 
 (defn read-csvs
   [files]
@@ -15,11 +15,11 @@
 (defn reorder
   [data]
   (let [cols (vec (sort (keys (first data))))]
-    (cons :gene_id (vec-remove (.indexOf cols :gene_id) cols))))
+    (cons :GENE_ID (vec-remove (.indexOf cols :GENE_ID) cols))))
 
 (defn merge-exp
-  "[[{:gene_id 'XXX0' :YYY0 1.2} {:gene_id 'XXX1' :YYY1 1.3}]
-    [{:gene_id 'XXX0' :YYY2 1.2} {:gene_id 'XXX1' :YYY3 1.3}]]"
+  "[[{:GENE_ID 'XXX0' :YYY0 1.2} {:GENE_ID 'XXX1' :YYY1 1.3}]
+    [{:GENE_ID 'XXX0' :YYY2 1.2} {:GENE_ID 'XXX1' :YYY3 1.3}]]"
   [all-exp-data]
   (apply map merge all-exp-data))
 
@@ -29,7 +29,7 @@
     (write-csv-by-cols! path row-data cols)))
 
 (defn merge-exp-files!
-  "Assumption: all files have the same gene_id list, no matter what order."
+  "Assumption: all files have the same GENE_ID list, no matter what order."
   [files path]
   (->> (read-csvs files)
        (merge-exp)
